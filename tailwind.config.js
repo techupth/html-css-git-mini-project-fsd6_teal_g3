6de771985336c8eb815a70a0655bc60b2a1b81da
boschaiyadej <boschaiyadej@gmail.com>
2024-05-07 09:55:33 +0700

    style: add font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "baloo-bhai": ["Baloo Bhai 2", "sans-serif"],
+        "dm-sans": ["DM Sans", "sans-serif"],
+        "paytone-one": ["Paytone One", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  // Other configuration...
+};
