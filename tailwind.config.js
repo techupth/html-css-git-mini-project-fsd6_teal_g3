e9c41284e49d2de30670b72aeb44c9b7f4b79894
tle <tlemakuz@gmail.com>
2024-05-07 14:37:36 +0700

    Feat: Footer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
